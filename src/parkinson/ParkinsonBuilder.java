ab809c93012ff2558bd7e66147bbb18024516c8a
Fabio03Rossi <fabio03.rossi@studenti.unicam.it>
2026-04-23 15:46:41 +0200

    Added some parameters

--- src/parkinson/ParkinsonBuilder.java
+++ src/parkinson/ParkinsonBuilder.java
@@ -8,6 +8,7 @@ import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.RandomCartesianAdder;
 import repast.simphony.space.continuous.WrapAroundBorders;
+import repast.simphony.parameter.Parameters;
 
 public class ParkinsonBuilder implements ContextBuilder<Object>{
 
@@ -15,14 +16,22 @@ public class ParkinsonBuilder implements ContextBuilder<Object>{
 	public Context<Object> build(Context<Object> context) {
 		context.setId("Parkinson");
 		
-		//Parameters params = RunEnvironment.getInstance().getParameters();
+		Parameters params = RunEnvironment.getInstance().getParameters();
+		
+		int spaceSize = Math.abs((Integer) params.getValue("space_size"));
+		int microNum = Math.abs((Integer) params.getValue("micro_num"));
+		int astroNum = Math.abs((Integer) params.getValue("astro_num"));
+		int neuroNum = Math.abs((Integer) params.getValue("neuro_num"));
+		
+		int actThr = Math.abs((Integer) params.getValue("activation_threshold"));
+		
 		
 		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder
 				.createContinuousSpaceFactory(null);
 		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace(
 				"space", context, new RandomCartesianAdder<Object>(),
-				new WrapAroundBorders(), 50,
-				50);
+				new WrapAroundBorders(), spaceSize,
+				spaceSize);
 		
 		RunEnvironment.getInstance().endAt(1000);
 
